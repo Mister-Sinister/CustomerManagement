a8efd40e75d5c88530914183d5dcb5d67203c5df
Ibrahim Adeshina <ibrahim.adeshina.10@gmail.com>
2022-06-08 00:01:40 +0100

    A one-to-one relationship between user profile and Customer model was established

--- accounts/views.py
+++ accounts/views.py
@@ -23,6 +23,7 @@ def registerPage(request):
             username = form.cleaned_data.get('username')
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(user=user)
             messages.success(request, 'Account was created for ' + username)
             return redirect('login')
     context = {'form': form}
@@ -68,10 +69,17 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
 
-
-    context = {}
+    print('orders:', orders)
+    context = {'orders': orders, 'total_orders': total_orders, 'delivered': delivered,
+               'pending': pending}
 
     return render(request, 'accounts/user.html', context)
 
